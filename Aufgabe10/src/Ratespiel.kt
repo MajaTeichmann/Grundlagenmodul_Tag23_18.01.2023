90b01b8e3814323067fde4e8f765d145d4031905
MajaTeichmann <katharina.nickname@hotmail.de>
2023-01-18 15:31:16 +0100

    bearbeitet

--- Aufgabe10/src/Ratespiel.kt
+++ Aufgabe10/src/Ratespiel.kt
@@ -11,10 +11,38 @@ import kotlin.random.Random
 // Ist die eingegebene Zahl gleich der Zahl des Computers, wird in der Konsole "Du hast gewonnen!" ausgegeben.
 // Wenn die eingegebene Zahl kleiner oder größer war (Der Spieler also falsch lag), wird die Runde wiederholt, solange
 // bis der Spieler die Zahl erraten hat.
-// b) Zähle jetzt zusätzlich auch die Anzahl an Versuche, die der Spieler gebraucht hat, bis er die Zahl des Computers erraten konnte.
-// c) Sei kreativ und erweitere dein Spiel wie du möchtest und mach es somit einzigartig.
+
+// todo: b) Zähle jetzt zusätzlich auch die Anzahl an Versuche, die der Spieler gebraucht hat, bis er die Zahl des
+//  Computers erraten konnte.
+
+// todo: c) Sei kreativ und erweitere dein Spiel wie du möchtest und mach es somit einzigartig.
 
 fun main() {
     val computerGuess = Random.nextInt(1, 100)
 
+    var versuche = 1
+    var versuchteZahlen = mutableListOf<Int>()
+
+    println("Bitte gib deinen Tipp ein!")
+    var playerGuess = readln().toInt()
+
+    while (playerGuess != computerGuess){
+        if(playerGuess in versuchteZahlen) {
+            println("Die Zahl hattest du schon! Versuche eine andere!")
+        }
+        if (playerGuess < computerGuess && playerGuess !in versuchteZahlen){
+            println("Zu klein!")
+            versuchteZahlen.add(playerGuess)
+            versuche++
+            }
+            else if(playerGuess > computerGuess && playerGuess !in versuchteZahlen){
+            println("Zu groß!")
+            versuchteZahlen.add(playerGuess)
+            versuche++
+            }
+        println("Bitte gib deinen Tipp ein!")
+        playerGuess = readln().toInt()
+    }
+    println("Du hast gewonnen!")
+    println("Du hast ${versuche} Versuche gebraucht!")
 }
